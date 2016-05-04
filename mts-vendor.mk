3b17f7cd63133fd3bab77aa546312cc8d5ab764b
SeriniTY320 <SeriniTY320@gmail.com>
2016-05-04 21:43:03 +0300

    Ебучий говнокод

---
+++ mts-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/mts/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/mts/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/mts/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/mts/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/mts/xbin,system/xbin)
+
